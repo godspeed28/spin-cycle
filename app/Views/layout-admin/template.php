0590985c8679915871243b41f5b60176d063f70d
godspeed28 <albertog4taz28@gmail.com>
2025-07-14 11:48:45 +0700

    add settings background

--- app/Views/layout-admin/template.php
+++ app/Views/layout-admin/template.php
@@ -33,6 +33,7 @@
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/assets/css/plugins.min.css" />
     <link rel="stylesheet" href="/assets/css/kaiadmin.min.css" />
+    <link rel="stylesheet" href="/assets/css/demo.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
@@ -56,11 +57,13 @@
     <script src="/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
     <script src="/assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
     <script src="/assets/js/plugin/jsvectormap/world.js"></script>
+    <script src="/assets/js/"></script>
     <script src="/assets/js/plugin/sweetalert/sweetalert.min.js"></script>
 
     <!-- Kaiadmin JS -->
     <script src="/assets/js/kaiadmin.min.js"></script>
     <script src="/assets/js/demo.js"></script>
+    <script src="assets/js/setting-demo.js"></script>
 
     <!-- SweetAlert2 CDN -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
